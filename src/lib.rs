7205dfee7907a1588821bdf746c3ea25104b51b4
Zahari Petkov <zahari@polydraw.com>
2015-03-15 11:09:10 +0200

    Trying out Rust benchmarking features.

--- src/lib.rs
+++ src/lib.rs
@@ -63,18 +63,3 @@ macro_rules! attr {
       }
    };
 }
-
-
-#[macro_export]
-macro_rules! write_html {
-    ($w:expr, ) => (());
-
-    ($w:expr, $e:tt) => (write!($w, "{}", $e));
-
-    ($w:expr, $tag:ident [ $($inner:tt)* ] $($rest:tt)*) => {{
-        write!($w, "<{}>", stringify!($tag));
-        write_html!($w, $($inner)*);
-        write!($w, "</{}>", stringify!($tag));
-        write_html!($w, $($rest)*);
-    }};
-}
