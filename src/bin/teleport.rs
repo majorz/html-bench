ca9f563bbc5e38deeaf2f381805bf3b116852923
Zahari Petkov <zahari@polydraw.com>
2015-03-16 02:00:42 +0200

    More template speed benchmarking.

--- src/bin/teleport.rs
+++ src/bin/teleport.rs
@@ -16,26 +16,21 @@ use std::iter::{repeat, FromIterator, IntoIterator};
 use teleport::{push_attribute, AttributeValue, Attribute};
 
 
-
 #[bench]
-fn string_concat(b: &mut Bencher) {
-
-   let mut table_contents: Vec<Vec<u32>> = Vec::new();
-   //let mut table_contents: Vec<[u32; 10]> = Vec::new();
-   for i in 0..10000 {
-      let k = i*10;
-      table_contents.push(Vec::from_iter((k..k+10)))
-      //table_contents.push([k, k+1, k+2, k+3, k+4, k+5, k+6, k+7, k+8, k+9]);
+fn concat_string_source(b: &mut Bencher) {
+   let mut table_contents: Vec<[&'static str; 10]> = Vec::new();
+   for _ in 0..1000 {
+      table_contents.push(["aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj"])
    }
 
    b.iter(|| {
-      let mut result = String::new();
+      let mut result = String::with_capacity(200000);
       result.push_str("<table>\n");
 
-      for index in 0..10000 {//table_contents.iter() {
+      for row in table_contents.iter() {
          result.push_str("  <tr>\n");
 
-         for item in table_contents[index].iter() {
+         for item in row {
             result.push_str("    <td>");
             result.push_str(&item.to_string());
             result.push_str("<td>\n");
@@ -45,28 +40,87 @@ fn string_concat(b: &mut Bencher) {
       }
 
       result.push_str("</table>");
+
+      result
    });
 }
 
 
+#[bench]
+fn concat_vector_source(b: &mut Bencher) {
+   let mut table_contents: Vec<[&'static str; 10]> = Vec::new();
+   for _ in 0..1000 {
+      table_contents.push(["aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj"])
+   }
+
+   b.iter(|| {
+      let mut result = Vec::new();
+      result.push("<table>\n");
+
+      for row in table_contents.iter() {
+         result.push("  <tr>\n");
+
+         for item in row {
+            result.push("    <td>");
+            result.push(item);
+            result.push("<td>\n");
+         }
+
+         result.push("  </tr>\n");
+      }
+
+      result.push("</table>");
+   });
+}
+
+
+fn render_table() {
+   let mut table_contents: Vec<[&'static str; 10]> = Vec::new();
+   for _ in 0..1000 {
+      table_contents.push(["aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj"])
+   }
+
+   let mut result = Vec::with_capacity(36000);
+   result.push("<table>\n");
+
+   for row in table_contents.iter() {
+      result.push("  <tr>\n");
+
+      for item in row {
+         result.push("    <td>");
+         result.push(item);
+         result.push("<td>\n");
+      }
+
+      result.push("  </tr>\n");
+   }
+
+   result.push("</table>");
+
+   println!("{}", result.len())
+}
+
+
 pub fn main() {
-   let class = AttributeValue::List(vec![
-      AttributeValue::StaticString("first"),
-      AttributeValue::StaticString("second")
-   ]);
+   render_table();
 
-   let first_attr = Attribute {
-      name: "class",
-      value: class,
-   };
-
-   let mut rendered = String::new();
-
-   push_attribute(&first_attr, &mut rendered);
-
-   println!("{}", rendered);
-
-   let list = attr!["id", "location"; "class", "identical"];
-
-   println!("{:?}", list);
+   // let class = AttributeValue::List(vec![
+   //    AttributeValue::StaticString("first"),
+   //    AttributeValue::StaticString("second")
+   // ]);
+   //
+   // let first_attr = Attribute {
+   //    name: "class",
+   //    value: class,
+   // };
+   //
+   // let mut rendered = String::new();
+   //
+   // push_attribute(&first_attr, &mut rendered);
+   //
+   // println!("{}", rendered);
+   //
+   // let list = attr!["id", "location"; "class", "identical"];
+   //
+   // println!("{:?}", list);
 }
